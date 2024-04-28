282866c25f74e32860cbb6b4c10ecdb54500a6c1
MuhammadFarrasThohari <farras.thohari@gmail.com>
2024-04-28 23:01:09 +0700

    Refactor navigation and authentication logic in App.js

--- App.js
+++ App.js
@@ -1,5 +1,48 @@
-import Router from "./Router";
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import { supabase } from "./lib/supabase";
 
-export default function App() {
-    return <Router />;
+const Stack = createNativeStackNavigator();
+
+export default function Router() {
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        const listener = supabase.auth.onAuthStateChange((event, session) => {
+            if (event === "SIGNED_IN") {
+                setSession(session);
+            }
+            if (event === "SIGNED_OUT") {
+                setSession(null);
+            }
+        });
+
+        return () => {
+            listener.unsubscribe();
+        };
+    }, []);
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {session ? (
+                    <Stack.Screen
+                        name="Home"
+                        component={Home}
+                        options={{ headerShown: false }}
+                    />
+                ) : (
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                        options={{ headerShown: false }}
+                    />
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
